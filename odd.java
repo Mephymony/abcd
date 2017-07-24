10ef69d5ae36fdd1c841859d4fd01087157c902c
Mephymony <mebinmony@gmail.com>
2017-07-24 12:13:58 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<=10;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+
